12d3bfdb1b5246f0704c8a93211cad75472a9be8
Bruno Beildeck <bbeildeck@gmail.com>
2014-12-06 16:28:46 -0200

    fix: Enabling more CORS headers

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var messages = [
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
